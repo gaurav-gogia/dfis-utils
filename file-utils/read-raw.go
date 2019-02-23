35c72bdb71965d77338ef4a7a4f37240b2b6d6c9
Gaurav Gogia <desmondmiles36@gmail.com>
2019-02-24 02:55:39 +0530

    low level function for reading raw bytes

---
+++ file-utils/read-raw.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"golang.org/x/sys/unix"
+)
+
+// raw function performs low level disk i/o reads and picks off raw bytes
+// use it for forensic purposes
+func raw() {
+	fd, err := unix.Open(ROOT, unix.O_RDONLY, 0777)
+	defer unix.Close(fd)
+	if err != nil {
+		panic(err)
+	}
+
+	buffer := make([]byte, 10)
+	if _, err := unix.Read(fd, buffer); err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Printf("Data: %b\n", buffer)
+}
