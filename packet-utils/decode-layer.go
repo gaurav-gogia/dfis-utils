427f8c907cc52879a56596dc6449355d4dc9a6e1
Gaurav Gogia <desmondmiles36@gmail.com>
2019-02-25 03:34:38 +0530

    capture and break down layers

---
+++ packet-utils/decode-layer.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+)
+
+func (box packetinfo) decode() {
+	var count int64
+	handle, _ := pcap.OpenLive(box.device, box.snaplen, box.promicious, box.timeout)
+	defer handle.Close()
+
+	src := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range src.Packets() {
+		count++
+		fmt.Printf("PACKET #%d\n", count)
+		unpack(packet)
+		fmt.Println("_______________________________________________________________________________")
+		fmt.Printf("\n")
+	}
+}
+
+func unpack(packet gopacket.Packet) {
+	ether := packet.Layer(layers.LayerTypeEthernet)
+	if ether != nil {
+		fmt.Println("Ethernet Layer Detected")
+		epack, _ := ether.(*layers.Ethernet)
+
+		fmt.Println("Source MAC: ", epack.SrcMAC)
+		fmt.Println("Destination MAC: ", epack.DstMAC)
+		fmt.Println("Ethernet Type: ", epack.EthernetType)
+	}
+
+	v4layer := packet.Layer(layers.LayerTypeIPv4)
+	if v4layer != nil {
+		fmt.Println("\nIPv4 Layer Detected")
+		ip, _ := v4layer.(*layers.IPv4)
+		fmt.Printf("From: %s to %s\n", ip.SrcIP, ip.DstIP)
+		fmt.Println("Protocol: ", ip.Protocol)
+	}
+
+	v6 := packet.Layer(layers.LayerTypeIPv6)
+	if v6 != nil {
+		fmt.Println("\nIPv6 Layer Detected")
+		ip, _ := v6.(*layers.IPv6)
+		fmt.Printf("From: %s to %s\n", ip.SrcIP, ip.DstIP)
+		fmt.Println("Class: ", ip.TrafficClass)
+	}
+
+	tcplayer := packet.Layer(layers.LayerTypeTCP)
+	if tcplayer != nil {
+		fmt.Println("\nTCP Layer Detected")
+		tcp, _ := tcplayer.(*layers.TCP)
+		fmt.Printf("From: %s to %s\n", tcp.SrcPort, tcp.DstPort)
+		fmt.Println("Sequ No: ", tcp.Seq)
+	}
+
+	fmt.Println("\nAll Layers:")
+	for _, layer := range packet.Layers() {
+		fmt.Println("-->", layer.LayerType())
+	}
+
+	applayer := packet.ApplicationLayer()
+	if applayer != nil {
+		fmt.Println("\nApplication Layer Detected")
+		fmt.Printf("%s\n", applayer.Payload())
+		if strings.Contains(string(applayer.Payload()), "http") {
+			fmt.Println("HTTP Found")
+		}
+	}
+}
