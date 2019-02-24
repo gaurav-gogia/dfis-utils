cf981605cb4172851d4f33242a7b72c6a5fb6e4b
Gaurav Gogia <desmondmiles36@gmail.com>
2019-02-24 15:04:14 +0530

    code management update

--- file-utils/main.go
+++ file-utils/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 // Global Constants
 const (
 	ROOT     = "/dev/disk2s1"
+	PATH     = "../../"
 	FILEPATH = "./read-raw.go"
 
 	IMAGE = 1 << iota
@@ -14,6 +16,13 @@ const (
 	AUDIO
 	ARCHIVE
 )
+
+type filenode struct {
+	fpath string
+	fname string
+	ftime time.Time
+	fsize int64
+}
 
 func main() {
 	fmt.Println("Reading raw bytes ....")
@@ -24,4 +33,10 @@ func main() {
 
 	fmt.Println("\nFinding files using magic numbers ....")
 	extract("../../", "./", IMAGE)
+
+	fmt.Println("\nPulling largest files out ....")
+	large(PATH)
+
+	fmt.Println("\nPulling recently modified files ....")
+	recent(PATH)
 }
