e711e63b1be3158a40a45ca64d566642a7d7df24
Gaurav Gogia <desmondmiles36@gmail.com>
2019-02-24 16:44:05 +0530

    list all available network devices with their info

---
+++ packet-utils/list-dev.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/google/gopacket/pcap"
+)
+
+func devlist() {
+	devices, _ := pcap.FindAllDevs()
+	for i, dev := range devices {
+		fmt.Println("DEVICE: ", i)
+		fmt.Println("Name: ", dev.Name)
+		fmt.Println("Flags: ", dev.Flags)
+		fmt.Println("Description: ", dev.Description)
+		fmt.Println("Addresses: ")
+		for _, addr := range dev.Addresses {
+			fmt.Println("--> IP: ", addr.IP)
+			fmt.Println("--> Broadcast: ", addr.Broadaddr)
+			fmt.Println("--> Netmask: ", addr.Netmask)
+			fmt.Println("--> P2P: ", addr.P2P)
+			fmt.Println()
+		}
+		fmt.Println()
+	}
+}
