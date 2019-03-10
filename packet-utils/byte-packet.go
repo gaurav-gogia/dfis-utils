d6d897718d03e972f477f7850b74df7f3767d204
Gaurav Gogia <desmondmiles36@gmail.com>
2019-03-11 03:22:24 +0530

    byte-packet conversion added

---
+++ packet-utils/byte-packet.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+)
+
+func conv(payload []byte) {
+	var options gopacket.SerializeOptions
+	buffer := gopacket.NewSerializeBuffer()
+
+	gopacket.SerializeLayers(
+		buffer,
+		options,
+		&layers.Ethernet{},
+		&layers.IPv4{},
+		&layers.TCP{},
+		gopacket.Payload(payload),
+	)
+
+	raw := buffer.Bytes()
+
+	ethpacket := gopacket.NewPacket(
+		raw,
+		layers.LayerTypeEthernet,
+		gopacket.Default,
+	)
+
+	ipacket := gopacket.NewPacket(
+		raw,
+		layers.LayerTypeIPv4,
+		gopacket.Default,
+	)
+
+	tcpacket := gopacket.NewPacket(
+		raw,
+		layers.LayerTypeTCP,
+		gopacket.Default,
+	)
+
+	fmt.Printf("Ethernet:\n%v\n", ethpacket)
+	fmt.Printf("IP:\n%v\n", ipacket)
+	fmt.Printf("TCP:\n%v\n", tcpacket)
+}
