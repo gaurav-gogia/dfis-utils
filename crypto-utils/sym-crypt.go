17249a59587af2919f84266d8fac193e17fa1c12
Gaurav Gogia <desmondmiles36@gmail.com>
2019-03-22 02:41:54 +0530

    symmetric encryption added

---
+++ crypto-utils/sym-crypt.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"io/ioutil"
+)
+
+func adves(path string) {
+	key := make([]byte, 32)
+	rand.Read(key)
+	fmt.Printf("AES Key: %s\n", hex.EncodeToString(key))
+
+	msg, err := ioutil.ReadFile(path)
+	handle(err)
+
+	cip, err := encode(key, msg)
+	handle(err)
+	fmt.Printf("Cipher Text: %s\n", hex.EncodeToString(cip))
+
+	txt, err := decode(key, cip)
+	handle(err)
+	fmt.Printf("Clear Text: %s\n", txt)
+}
+
+func encode(key, msg []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	cip := make([]byte, len(msg)+aes.BlockSize)
+	iv := cip[:aes.BlockSize]
+	_, err = io.ReadFull(rand.Reader, iv)
+	if err != nil {
+		return nil, err
+	}
+
+	cfb := cipher.NewCFBDecrypter(block, iv)
+	cfb.XORKeyStream(cip[aes.BlockSize:], msg)
+
+	return cip, nil
+}
+
+func decode(key, cip []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	//txt := make([]byte, len(cip)+aes.BlockSize)
+	iv := cip[:aes.BlockSize]
+	cip = cip[aes.BlockSize:]
+
+	cfb := cipher.NewCFBDecrypter(block, iv)
+	cfb.XORKeyStream(cip, cip)
+
+	return cip, nil
+}
