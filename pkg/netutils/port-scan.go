30f587cdb923dd184c4a6e13274aa37d598d2c0c
Gaurav Gogia <desmondmiles36@gmail.com>
2019-10-27 00:41:57 +0530

    port scan logic update

--- pkg/netutils/port-scan.go
+++ pkg/netutils/port-scan.go
@@ -31,12 +31,14 @@ func Scan(ipaddr string, startPort, endPort int) {
 
 func tcptest(ipaddr string, port int, done chan bool) {
 	path := fmt.Sprintf("%s:%d", ipaddr, port)
-	_, err := net.DialTimeout("tcp", path, time.Second*10)
-
-	if err != nil {
+	if conn, err := net.DialTimeout("tcp", path, time.Second*10); err != nil {
 		fmt.Printf("Port: %d, IP: %s | CLOSE\n", port, ipaddr)
 	} else {
-		fmt.Printf("Port: %d, IP: %s | OPEN\n", port, ipaddr)
+		if err := conn.Close(); err != nil {
+			fmt.Printf("Port: %d, IP: %s | CLOSE\n", port, ipaddr)
+		} else {
+			fmt.Printf("Port: %d, IP: %s | OPEN\n", port, ipaddr)
+		}
 	}
 
 	done <- true
